a6f125d00bc6014c93d9a3f615b262c2b461af96
Nick Zou <nick@elevate.ca>
2022-12-01 23:35:25 -0500

    fix console log messages

--- sharp.config.js
+++ sharp.config.js
@@ -3,6 +3,7 @@ const glob = require('glob')
 const {basename} = require('path')
 const { existsSync, mkdir } = require('fs')
 const sharp = require('sharp')
+const clc = require('cli-color')
 
 dotenv.config()
 
@@ -14,31 +15,32 @@ const images = glob.sync('./src/images/*.{jpg,jpeg,png,gif}')
 const sizes = [1536, 1280, 1024, 768, 680]
 
 if (images.length > 0) {
-    images.every(async i => {
-        const image = await sharp(i)
-        const metaData = await image.metadata()
-        const format = metaData.format
-        const width = metaData.width
-        const fileName = basename(i, `.${format}`)
-        if(!existsSync(outDir)) {
-            mkdir(outDir, (err) => {
-                if (err) {
-                    return console.error(err);
-                }
-                console.log('Directory created successfully!')
-            })
+  if(!existsSync(outDir)) {
+    mkdir(outDir, (err) => {
+        if (err) {
+            return console.error(err);
         }
-        sizes.forEach(size => {
-            image.toFile(`${outDir}/${fileName}.${format}`)
-            if (width > size) {
-                image
-                .resize({
-                    width: size
-                })
-                .toFile(`${outDir}/${fileName}-${size}.${format}`)
-            }
-        })
+        console.log(clc.green(`Directory created successfully!`))
     })
+  }
+  images.every(async i => {
+      const image = await sharp(i)
+      const metaData = await image.metadata()
+      const format = metaData.format
+      const width = metaData.width
+      const fileName = basename(i, `.${format}`)
+      sizes.forEach(size => {
+          image.toFile(`${outDir}/${fileName}.${format}`)
+          if (width > size) {
+              image
+              .resize({
+                  width: size
+              })
+              .toFile(`${outDir}/${fileName}-${size}.${format}`)
+              console.log(clc.green(`${fileName}-${size} created successfully!`))
+          }
+      })
+  })
 } else {
-    console.log('images folder is empty')
+    console.log(clc.yellow(`images folder is empty`))
 }
